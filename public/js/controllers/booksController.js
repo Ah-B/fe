deac197d2518e1c3b619d2dca139eb8c04bfa955
Abdelhedi Belghith <blg.abdelhedi@gmail.com>
2016-12-21 17:39:56 +0100

    last template books + authors

--- public/js/controllers/booksController.js
+++ public/js/controllers/booksController.js
@@ -1,9 +1,15 @@
 app.controller('booksController', function($scope, $http) {
     $scope.bookOrder = "title";
+    $scope.changeGenre = function(test){
+      if(test =='All'){
+        $scope.genreFilter = "";
+      }
+        $scope.genreFilter = test;
+    }
 
     $http.get('/api/book/').success(function(data) {
         $scope.books = data;
-        $scope.genres = [];
+        $scope.genres = ['All'];
         for (book of data) {
             if ($scope.genres.indexOf(book.genre) === -1) {
                 $scope.genres.push(book.genre)
